42afc03969604dc8230be088ba32514879fb857b
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2024-08-28 23:40:33 -0300

    Add: new start

---
+++ src/main/java/com/fardragi/dragiutils/DragiUtils.java
@@ -0,0 +1,51 @@
+package com.fardragi.dragiutils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+
+@Mod(
+    modid = DragiUtils.MODID,
+    version = Tags.VERSION,
+    name = "Dragi",
+    acceptedMinecraftVersions = "[1.7.10]",
+    acceptableRemoteVersions = "*")
+public class DragiUtils {
+
+    public static final String MODID = "dragiutils";
+    public static final Logger LOG = LogManager.getLogger(MODID);
+
+    @SidedProxy(clientSide = "com.fardragi.dragiutils.ClientProxy", serverSide = "com.fardragi.dragiutils.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
+    // GameRegistry." (Remove if not needed)
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+
+    @Mod.EventHandler
+    // register server commands in this event handler (Remove if not needed)
+    public void serverStarting(FMLServerStartingEvent event) {
+        proxy.serverStarting(event);
+    }
+}
